f1ec72d2b2be76fa9d3ef591460299bb64fea38f
Arif Hasanov <arifhasanov@yahoo.com>
2021-11-11 11:25:34 +0100

    Done

---
+++ Lesson 4_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for fibonachi calculation:");
+            int number = int.Parse(Console.ReadLine());
+            int fibonachiResult = Fibonachi(number);
+            Console.WriteLine("Fibonachi of " + number + " is " + fibonachiResult.ToString());
+        }
+
+        public static int Fibonachi(int number)
+        {
+            if (number <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonachi(number - 1) + Fibonachi(number - 2);
+            }
+        }
+    }
+}
